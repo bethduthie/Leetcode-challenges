ccfe6e84c2cb38db6c520c03166862094bdf13ee
bethduthie <bethanyduthie@gmail.com>
2020-05-27 16:22:13 +0100

    Completed 'Ugly numbers' challenge, began 'Max Depth of Binary Tree' challenge

---
+++ ugly_numbers.py
@@ -0,0 +1,11 @@
+def isUgly(num):
+    if num == 1:
+        return True
+    if num <=0:
+        return False
+    for i in [2,3,5]:
+        while num % i == 0:
+            num /= i
+    return num == 1
+
+
